10bae0bd538d24054ffa575707b23b38235e11c2
ajayj <ajayj@DESKTOP-POP6H1C>
2017-11-20 21:56:22 +0530

    Intial commit newtours

---
+++ Newtours/src/testCases/Hello.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+
+	public static void addTwoNumbers(int a, int b) {
+		int c = a + b;
+		System.out.println("C value:" + c);
+	}
+
+}
---
+++ Newtours/src/testCases/Sample.java
@@ -0,0 +1,17 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void test1() {
+		Hello.addTwoNumbers(10, 20);
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("not specified");
+	}
+
+}
